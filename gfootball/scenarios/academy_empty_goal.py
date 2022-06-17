4efadfaadcc02991a5917f86f63c241757789d99
Giorgos Moutsopoulos <georgemuts@gmail.com>
2022-06-17 17:10:30 +0300

    Add files via upload

--- gfootball/scenarios/academy_empty_goal.py
+++ gfootball/scenarios/academy_empty_goal.py
@@ -18,21 +18,27 @@
 
 
 from . import *
+import random
 
 
+#rand = random.uniform(-0.3,0.3)
 def build_scenario(builder):
   builder.config().game_duration = 400
   builder.config().deterministic = False
   builder.config().offsides = False
   builder.config().end_episode_on_score = True
   builder.config().end_episode_on_out_of_play = True
-  builder.config().end_episode_on_possession_change = True
+
+  #builder.config().end_episode_on_possession_change = True   #AN XASO TIN MPALA SYNEXIZOUN NA PAIZOUN
+
+  builder.config().right_team_difficulty = 0.0
+  #builder.config().left_team_difficulty = 0.0
   builder.SetBallPosition(0.02, 0.0)
 
   builder.SetTeam(Team.e_Left)
-  builder.AddPlayer(-1.0, 0.0, e_PlayerRole_GK)
-  builder.AddPlayer(0.0, 0.0, e_PlayerRole_CB)
+  builder.AddPlayer(0.0, 0.0, e_PlayerRole_GK)
+  #builder.AddPlayer(0.0, 0.0, e_PlayerRole_LM)
 
   builder.SetTeam(Team.e_Right)
-  builder.AddPlayer(-1.0, 0.0, e_PlayerRole_GK)
-  print("Inside scenario build")
+  #builder.AddPlayer(-1.0, 0.0, e_PlayerRole_GK)
+  builder.AddPlayer(-1.000000, 0.000000, e_PlayerRole_GK)
